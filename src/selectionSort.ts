808e05845bbd2b57675e362be92be1414925c4a9
Levi Wallace <guysmiley.levi@gmail.com>
2019-10-20 17:39:14 -0700

    selectionSort + deleted map

--- src/selectionSort.ts
+++ src/selectionSort.ts
@@ -13,22 +13,17 @@ function bubblePass(xs: number[], n: number): void {
     requestAnimationFrame(() => bubblePass(xs, n+1));
 }
 
-function selectionPass(xs: number[], i: number, n: number): void {
-    if (n & 10) {
-        let indx = i;
-        for (let j = i+1; j < xs.length; j++) {
-            if (xs[indx] > xs[j]) {
-                indx = j;
-           }
+function selectionSort(xs: number[]): void {
+    for(let i = 0; i < xs.length-1; i++) {
+        let idx = i;
+        for(let j = i; j < xs.length; j++) {
+            if(xs[idx] > xs[j])
+                idx = j;
         }
-        swap(xs, indx, i);
+        if (idx == i)
+            swap(xs, i, idx)
     }
-    if (i < xs.length-1)
-        i+1
-    draw(xs);
-    requestAnimationFrame(() => selectionPass(xs, i, n+1));
 }
-
 // func01
 //
 // requestAimationFrame(func01);
@@ -37,10 +32,12 @@ function selectionPass(xs: number[], i: number, n: number): void {
 
 function main(): void {
     let out: number[] = []
-    for(let i = 0; i < 600; i ++) {
+    for(let i = 0; i < 10; i ++) {
         out.push(Math.floor(Math.random()*100));
     }
-    console.log("test")
+    let out2 = [3, 5, 1, 2];
+    selectionSort(out2);
+    console.log(out2);
 }
 
 main();
